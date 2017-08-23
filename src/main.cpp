00d95f624e8676bea4e3bae5020a8a504bc15a4f
Li-Lichen Zheng <ny8859884@gmail.com>
2017-08-23 17:56:36 +0800

    rename Source to main

---
+++ src/main.cpp
@@ -0,0 +1,16 @@
+#include "Gate.h"
+#include "Network.h"
+#include <fstream>
+#include <iostream>
+using namespace std;
+
+string filename;
+
+int main(int argc, char *argv[]) {
+
+  // Network network;
+  // network.parser();
+  // network.topologySort();
+  // network.test();
+  return 0;
+}
