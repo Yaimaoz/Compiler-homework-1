69351e7caa61937e8d585a0555d78b1e50e8a997
Li-Lichen Zheng <ny8859884@gmail.com>
2017-08-24 16:35:31 +0800

    add draw Circle & draw Rectangle

--- src/ScalableVectorGraphicsGenerator.cpp
+++ src/ScalableVectorGraphicsGenerator.cpp
@@ -1,7 +1,42 @@
-#include <ScalableVectorGraphicsGenerator.hpp>
-
+#include "ScalableVectorGraphicsGenerator.hpp"
+using std::endl;
 ScalableVectorGraphicsGenerator::ScalableVectorGraphicsGenerator() {}
 
 ScalableVectorGraphicsGenerator::~ScalableVectorGraphicsGenerator() {}
 
-void ScalableVectorGraphicsGenerator::SVGFilePrototype() {}
+void ScalableVectorGraphicsGenerator::SVGFilePrototype()
+{
+    Vec3 temp1 = {0, 0, 255}, temp2 = {0, 0, 0};
+    out << "<svg width=\"400px\" height=\"180px\">" << endl;
+    drawCircle(50, 50, 40, temp1, 3, temp2);
+    out << "</svg>" << endl;
+}
+
+void ScalableVectorGraphicsGenerator::drawRectangle(int x, int y, int width, int height, Vec3 fillRGB, int strokeWidth, Vec3 strokeRGB)
+{
+    out << "\t<rect "
+        << "x=\"" << x << "\" "
+        << "y=\"" << y << "\" "
+        << "width=\"" << width << "\" "
+        << "height=\"" << height << "\" ";
+    out << "style=\""
+        << "fill:rgb(" << fillRGB[0] << "," << fillRGB[1] << "," << fillRGB[2] << ");"
+        << "stroke-width:" << strokeWidth << ";"
+        << "stroke:rgb(" << strokeRGB[0] << "," << strokeRGB[1] << "," << strokeRGB[2] << ");"
+        << "\"";
+    out << " />" << endl;
+}
+
+void ScalableVectorGraphicsGenerator::drawCircle(int cx, int cy, int r, Vec3 fillRGB, int strokeWidth, Vec3 strokeRGB)
+{
+    out << "\t<circle "
+        << "cx=\"" << cx << "\" "
+        << "cy=\"" << cy << "\" "
+        << "r=\"" << r << "\" ";
+    out << "style=\""
+        << "fill:rgb(" << fillRGB[0] << "," << fillRGB[1] << "," << fillRGB[2] << ");"
+        << "stroke-width:" << strokeWidth << ";"
+        << "stroke:rgb(" << strokeRGB[0] << "," << strokeRGB[1] << "," << strokeRGB[2] << ");"
+        << "\"";
+    out << " />" << endl;
+}
--- src/ScalableVectorGraphicsGenerator.hpp
+++ src/ScalableVectorGraphicsGenerator.hpp
@@ -1,14 +1,29 @@
 #pragma once
 #include <fstream>
+#include <list>
+#include <string>
+#include <vector>
+extern std::string filename;
+extern std::ofstream out;
 
-enum Type { WIRE, BUFFER, AND, OR, NOT, NAND, NOR, XOR, NXOR };
+enum Type { WIRE,
+            BUFFER,
+            AND,
+            OR,
+            NOT,
+            NAND,
+            NOR,
+            XOR,
+            NXOR };
+typedef unsigned int Vec3[3];
 class ScalableVectorGraphicsGenerator {
 
 public:
-  ScalableVectorGraphicsGenerator();
-  ~ScalableVectorGraphicsGenerator();
-  void SVGFilePrototype();
+    ScalableVectorGraphicsGenerator();
+    ~ScalableVectorGraphicsGenerator();
+    void SVGFilePrototype();
+    void drawRectangle(int, int, int, int, Vec3, int, Vec3);
+    void drawCircle(int, int, int, Vec3, int, Vec3);
 
 private:
-  Type type;
 };
