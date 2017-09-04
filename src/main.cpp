10c64221ea599ef6a4775aaf0b73242fe4db7826
Li-Lichen Zheng <ny8859884@gmail.com>
2017-09-04 15:21:57 +0800

    [Test] Conbine Network, SVGGen

--- src/main.cpp
+++ src/main.cpp
@@ -17,15 +17,30 @@ int main(int argc, char* argv[])
     }
     filename.assign(buf2.rbegin(), buf2.rend());
 
-    out.open(filename + ".html", std::ios::out);
-    ScalableVectorGraphicsGenerator test(1000, 1000);
-    test.SVGFilePrototype();
-    out.close();
-
     Network network;
     network.parser(buf1);
     network.topologySort();
     network.test();
-    network.printGraph();
+    network.breadthFirstSearch();
+    network.printLevel();
+    out.open(filename + ".html", std::ios::out);
+
+    ScalableVectorGraphicsGenerator test;
+    test.SVGDeclare(1000, 1000);
+    test.setGateSize(50);
+    int spacing = 50;
+    int x = 0, y = 0;
+    for (auto level : network.levelTable) {
+        y = 0;
+        for (auto g : level.second) {
+            test.drawOR(x, y);
+            y += spacing * 2;
+        }
+        x += spacing * 2;
+    }
+
+    test.SVGEnd();
+    out.close();
+
     return 0;
 }
