ec11f072872d288d12929316716aa849711c2897
Li-Lichen Zheng <ny8859884@gmail.com>
2017-09-04 11:41:28 +0800

    [Add] class Gate add a new member "level"

--- src/Gate.cpp
+++ src/Gate.cpp
@@ -1,34 +1,41 @@
-#include"Gate.h"
+#include "Gate.hpp"
 
-Gate::Gate():value(-1),type(NONE),isTrav(false){}
+Gate::Gate()
+    : value(-1)
+    , type(NONE)
+    , isTrav(false)
+    , level(1000)
+{
+}
 
-void Gate::eval(){
+void Gate::eval()
+{
 
-    switch(type){
-    
-        case BUFFER:
-            value = fanins.front()->value;
-            break;
-        case AND:
-            value = fanins.front()->value && fanins.back()->value;
-            break;
-        case OR:
-            value = fanins.front()->value || fanins.back()->value;
-            break;
-        case NOT:
-            value = !fanins.front()->value;
-            break;
-        case NAND:
-            value = !(fanins.front()->value && fanins.back()->value);
-            break;
-        case NOR:
-            value = !(fanins.front()->value || fanins.back()->value);
-            break;
-        case XOR:
-            value = fanins.front()->value != fanins.back()->value;
-            break;
-        case NXOR:
-            value = fanins.front()->value == fanins.back()->value;
-            break;
+    switch (type) {
+
+    case BUFFER:
+        value = fanins.front()->value;
+        break;
+    case AND:
+        value = fanins.front()->value && fanins.back()->value;
+        break;
+    case OR:
+        value = fanins.front()->value || fanins.back()->value;
+        break;
+    case NOT:
+        value = !fanins.front()->value;
+        break;
+    case NAND:
+        value = !(fanins.front()->value && fanins.back()->value);
+        break;
+    case NOR:
+        value = !(fanins.front()->value || fanins.back()->value);
+        break;
+    case XOR:
+        value = fanins.front()->value != fanins.back()->value;
+        break;
+    case NXOR:
+        value = fanins.front()->value == fanins.back()->value;
+        break;
     }
 }
--- src/Gate.hpp
+++ src/Gate.hpp
@@ -1,26 +1,35 @@
 #pragma once
-#include<string>
-#include<cstring>
-#include<list>
-#include<iostream>
+#include <cstring>
+#include <iostream>
+#include <list>
+#include <string>
 class Gate;
-typedef std::list<Gate*> GateList;
+typedef std::list< Gate* > GateList;
 
-enum GateType{ NONE, BUFFER, AND, OR, NOT, NAND, NOR, XOR, NXOR, INPUT, OUTPUT };
-class Gate{
+enum GateType { NONE,
+                BUFFER,
+                AND,
+                OR,
+                NOT,
+                NAND,
+                NOR,
+                XOR,
+                NXOR,
+                INPUT,
+                OUTPUT };
+class Gate {
 
-    public:
-        Gate();
-        void eval();
+public:
+    Gate();
+    void eval();
 
-        
-        std::string name;
-        GateList fanins;
-        GateList fanouts;
-        int value;
-        GateType type;
-        bool isTrav;
+    std::string name;
+    GateList fanins;
+    GateList fanouts;
+    int value;
+    GateType type;
+    bool isTrav;
+    size_t level;
 
-        GateList::iterator fanOutIt;
+    GateList::iterator fanOutIt;
 };
-
