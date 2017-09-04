182c11f01d7df7e3f22711e175cee49ab6bae41e
Li-Lichen Zheng <ny8859884@gmail.com>
2017-09-04 10:34:08 +0800

    [Add] class Gate

---
+++ src/Gate.cpp
@@ -0,0 +1,34 @@
+#include"Gate.h"
+
+Gate::Gate():value(-1),type(NONE),isTrav(false){}
+
+void Gate::eval(){
+
+    switch(type){
+    
+        case BUFFER:
+            value = fanins.front()->value;
+            break;
+        case AND:
+            value = fanins.front()->value && fanins.back()->value;
+            break;
+        case OR:
+            value = fanins.front()->value || fanins.back()->value;
+            break;
+        case NOT:
+            value = !fanins.front()->value;
+            break;
+        case NAND:
+            value = !(fanins.front()->value && fanins.back()->value);
+            break;
+        case NOR:
+            value = !(fanins.front()->value || fanins.back()->value);
+            break;
+        case XOR:
+            value = fanins.front()->value != fanins.back()->value;
+            break;
+        case NXOR:
+            value = fanins.front()->value == fanins.back()->value;
+            break;
+    }
+}
